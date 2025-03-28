fa068555656ae808bd7910d1b43848b074ce1785
yaoguyuan <yaoguyuan53@163.com>
2025-03-28 21:30:16 +0800

    Random greeting

--- greetings/hello.go
+++ greetings/hello.go
@@ -3,12 +3,22 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func SayHello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hello, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
